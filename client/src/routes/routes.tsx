7cf8f8c29f7971d6e23c69f1b79c681534293c1f
liferovich <56970453+liferovich@users.noreply.github.com>
2022-05-14 13:13:50 +0300

    basic func

---
+++ client/src/routes/routes.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Cart from '../pages/Cart';
+import CategoryNeon from '../pages/categories/CategoryNeon';
+import Catalog from '../pages/Catalog';
+import HomePage from '../pages/HomePage';
+import Product from '../pages/Product';
+import WishList from '../pages/WishList';
+import News from '../pages/News';
+import About from '../pages/About';
+import Contacts from '../pages/Contacts';
+
+export function useRoutes() {
+    return (
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/catalog/neon' element={<CategoryNeon />} />
+            <Route path='/product/:id' element={<Product />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/wishlist' element={<WishList />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contacts' element={<Contacts />} />
+            <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+    );
+}
